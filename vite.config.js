3661ff28b07f0de170796d36d1fd5751990abdd8
tariqmalik <tariqmalik1258@gmail.com>
2025-11-06 04:57:22 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/",  // Ye important hai custom domain ke liye
+  base: "./", // Important for GitHub Pages / custom domain
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  },
   build: {
     outDir: "dist",
     emptyOutDir: true
