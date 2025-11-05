246651de03bfa183b4e8f2d2b49682c994e74a1a
tariqmalik <tariqmalik1258@gmail.com>
2025-11-06 04:59:49 +0500

    fi

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // Important for GitHub Pages / custom domain
+  base: "./", // Relative paths for GitHub Pages
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src") // remove "./" before src
+    }
+  },
   build: {
     outDir: "dist",
     emptyOutDir: true
