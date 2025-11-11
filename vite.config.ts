21e38e5e226d31d1022a553263250146b202c1f7
tariqmalik <tariqmalik1258@gmail.com>
2025-11-11 06:12:09 +0500

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
+import { componentTagger } from "lovable-tagger"
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio-website/", // important for GitHub Pages
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +15,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}))
